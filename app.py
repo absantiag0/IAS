849806fcc2fc45a4c0a445d48d72a9f7fc7620e9
YovHer <carlos.munos@mercadolibre.com.mx>
2022-03-03 17:37:43 -0600

    Iniciar el Flask

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import *
+#import pymysql #$ python3 -m pip install PyMySQL
+
+
+app = Flask(__name__)
+
+#MySQL Connection
+# Connect to the database
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='',
+#                              database='IAS',
+#                              cursorclass=pymysql.cursors.DictCursor)
+
+# settings
+app.secret_key = 'mysecretkey'
+@app.route('/')
+def Index():
+#   try:
+#     if 'FullName' in session:
+    #   return redirect('/home')
+    # else:
+    return render_template('home.html')
+#   except Exception as error:
+#     flash(str(error))
+#     return render_template('index.html')
+#     return render_template('index.html',usuarios = data)
+
+
+
+if __name__=='__main__':
+    app.run(port = 4000, debug =True)
